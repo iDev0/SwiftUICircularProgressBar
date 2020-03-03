8073d4325e6792a75621bfd7826033c8ea427c91
SamplesMaker <insert@kakao.com>
2020-03-03 13:08:03 +0900

    Add Sample

--- CircularProgressBar/ContentView.swift
+++ CircularProgressBar/ContentView.swift
@@ -9,8 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var progress: CGFloat = 0.0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 20) {
+            HStack {
+                Text("0%")
+                Slider(value: $progress)
+                Text("100%")
+            }.padding()
+            
+            ZStack {
+                // 3.
+                Circle()
+                    .stroke(Color.gray, lineWidth: 20)
+                    .opacity(0.1)
+                // 4.
+                Circle()
+                    .trim(from: 0, to: progress)
+                    .stroke(Color.red, lineWidth: 20)
+                    .rotationEffect(.degrees(-90))
+                // 5.
+                .overlay(
+                    Text("\(Int(progress * 100.0))%"))
+                     
+            }.padding(20)
+            .frame(height: 300)
+            
+            Spacer()
+        }
     }
 }
 
